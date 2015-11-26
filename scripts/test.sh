8d9c7d08409ee8339c1e06b2c811b3363ea9e7f9
Sebastian Cruz <cruzseba@amazon.com>
2015-11-26 16:30:30 +0000

    Second try

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
+[ -t <&0 ] || exec >> /tmp/test.log
 
 echo "test"
+>&2 echo "error"
 env
 SCRIPTSDIR="$(dirname "$(realpath "$0")")"
 echo -e "\n\n\nDIR=$SCRIPTSDIR\n\n\n"
