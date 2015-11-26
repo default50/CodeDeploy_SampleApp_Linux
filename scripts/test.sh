60dcf52364bb0c0fc4784dd25a3f5ac019df9c36
Sebastian Cruz <cruzseba@amazon.com>
2015-11-26 16:49:08 +0000

    4th try

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,9 @@ exec 3>&1 4>&2 >/tmp/$0.log 2>&1
 
 echo "test"
 >&2 echo "error"
-env
-SCRIPTSDIR="$(dirname "$(realpath "$0")")"
-echo -e "\n\n\nDIR=$SCRIPTSDIR\n\n\n"
-su -c "bash -l $SCRIPTSDIR/test2.sh" - ec2-user
+#env
+#SCRIPTSDIR="$(dirname "$(realpath "$0")")"
+#echo -e "\n\n\nDIR=$SCRIPTSDIR\n\n\n"
+#su -c "bash -l $SCRIPTSDIR/test2.sh" - ec2-user
 
 exec 1>&3 2>&4
