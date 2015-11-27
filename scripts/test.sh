5653e6cd029418bad98336b7d07b6cb9bb72da23
Sebastian Cruz <cruzseba@amazon.com>
2015-11-27 07:52:25 +0000

    Reverted test.sh to simple script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-set -x
+#set -x
 
-echo "test"
->&2 echo "error"
-#env
-#SCRIPTSDIR="$(dirname "$(realpath "$0")")"
-#echo -e "\n\n\nDIR=$SCRIPTSDIR\n\n\n"
-#su -c "bash -l $SCRIPTSDIR/test2.sh" - ec2-user
+echo "env test"
+
+env
