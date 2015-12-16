99816e9e6a8d243ff38bb226db9d49b68c50b106
Sebastian Cruz <cruzseba@amazon.com>
2015-12-16 09:34:46 +0000

    Moved common_functions -> asg

--- scripts/protect_asg.sh
+++ scripts/protect_asg.sh
@@ -38,14 +38,6 @@ if [ $? == 0 -a -n "$asg" ]; then
         error_exit "CLI must be at least version ${MIN_CLI_X}.${MIN_CLI_Y}.${MIN_CLI_Z} to work with AutoScaling Standby"
     fi
 
-    msg "Attempting to move instance out of Standby"
-    autoscaling_exit_standby $INSTANCE_ID $asg
-    if [ $? != 0 ]; then
-        error_exit "Failed to move instance out of standby"
-    else
-        msg "Instance is no longer in Standby"
-    fi
-
     msg "Attempting to remove instance from Protected From Scale In"
     autoscaling_unset_protected $INSTANCE_ID $asg
     if [ $? != 0 ]; then
@@ -56,38 +48,7 @@ if [ $? == 0 -a -n "$asg" ]; then
     fi
 fi
 
-msg "Instance is not part of an ASG, continuing..."
-
-msg "Checking that user set at least one load balancer"
-if test -z "$ELB_LIST"; then
-    error_exit "Must have at least one load balancer to register to"
-fi
-
-# Loop through all LBs the user set, and attempt to register this instance to them.
-for elb in $ELB_LIST; do
-    msg "Checking validity of load balancer named '$elb'"
-    validate_elb $INSTANCE_ID $elb
-    if [ $? != 0 ]; then
-        msg "Error validating $elb; cannot continue with this LB"
-        continue
-    fi
-
-    msg "Registering $INSTANCE_ID to $elb"
-    register_instance $INSTANCE_ID $elb
-
-    if [ $? != 0 ]; then
-        error_exit "Failed to register instance $INSTANCE_ID from ELB $elb"
-    fi
-done
-
-# Wait for all Registrations to finish
-msg "Waiting for instance to register to its load balancers"
-for elb in $ELB_LIST; do
-    wait_for_state "elb" $INSTANCE_ID "InService" $elb
-    if [ $? != 0 ]; then
-        error_exit "Failed waiting for $INSTANCE_ID to return to $elb"
-    fi
-done
+msg "Instance is not part of an ASG, nothing to do."
 
 msg "Finished $(basename $0) at $(/bin/date "+%F %T")"
 
--- scripts/unprotect_asg.sh
+++ scripts/unprotect_asg.sh
@@ -46,49 +46,9 @@ if [ $? == 0 -a -n "$asg" ]; then
         msg "Instance is protected"
         exit 0
     fi
-
-    msg "Attempting to put instance into Standby"
-    autoscaling_enter_standby $INSTANCE_ID $asg
-    if [ $? != 0 ]; then
-        error_exit "Failed to move instance into standby"
-    else
-        msg "Instance is in standby"
-        exit 0
-    fi
 fi
 
-msg "Instance is not part of an ASG, continuing..."
-
-msg "Checking that user set at least one load balancer"
-if test -z "$ELB_LIST"; then
-    error_exit "Must have at least one load balancer to deregister from"
-fi
-
-# Loop through all LBs the user set, and attempt to deregister this instance from them.
-for elb in $ELB_LIST; do
-    msg "Checking validity of load balancer named '$elb'"
-    validate_elb $INSTANCE_ID $elb
-    if [ $? != 0 ]; then
-        msg "Error validating $elb; cannot continue with this LB"
-        continue
-    fi
-
-    msg "Deregistering $INSTANCE_ID from $elb"
-    deregister_instance $INSTANCE_ID $elb
-
-    if [ $? != 0 ]; then
-        error_exit "Failed to deregister instance $INSTANCE_ID from ELB $elb"
-    fi
-done
-
-# Wait for all Deregistrations to finish
-msg "Waiting for instance to de-register from its load balancers"
-for elb in $ELB_LIST; do
-    wait_for_state "elb" $INSTANCE_ID "OutOfService" $elb
-    if [ $? != 0 ]; then
-        error_exit "Failed waiting for $INSTANCE_ID to leave $elb"
-    fi
-done
+msg "Instance is not part of an ASG, nothing to do."
 
 msg "Finished $(basename $0) at $(/bin/date "+%F %T")"
 
