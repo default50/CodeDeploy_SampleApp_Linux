fffe14c75941c07cfc085b60f7c7fbdea0011bc8
Sebastian Cruz <cruzseba@amazon.com>
2015-12-25 14:27:13 +0000

    Print messages

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,8 +6,11 @@ if which yum &>/dev/null; then
   chkconfig httpd on
 elif which apt-get &>/dev/null; then
   if ! dpkg-query -l apache2 | grep -q "^ii"; then
+    echo "apache2 not installed, installing!"
     apt-get update
     apt-get -y upgrade
     apt-get -y install apache2
+  else
+    echo "apache2 already installed, skipping!"
   fi
 fi
