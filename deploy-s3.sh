5d71a0e27a42b69fc452b89c623692ab94c5d28e
Sebastian Cruz <cruzseba@amazon.com>
2016-01-21 15:18:45 +0000

    Ignore hidden files in bundle

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -8,5 +8,5 @@ bucket_key="CodeDeploy"
 configname="CodeDeployDefault.OneAtATime"
 profile="code"
 
-aws deploy push --profile ${profile} --application-name ${appname} --s3-location s3://${bucket}/${bucket_key}/${zipname} --description ${appname}
+aws deploy push --profile ${profile} --application-name ${appname} --s3-location s3://${bucket}/${bucket_key}/${zipname} --ignore-hidden-files --description ${appname}
 aws deploy create-deployment --profile ${profile} --application-name ${appname} --deployment-config-name ${configname} --deployment-group-name ${dgname} --s3-location bucket=${bucket},bundleType=zip,key=${bucket_key}/${zipname}
