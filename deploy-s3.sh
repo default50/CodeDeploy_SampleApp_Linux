062a8f7bc55456bf347366a6bb9571c5c0454387
Sebastian Cruz <cruzseba@amazon.com>
2016-01-21 12:27:54 +0000

    Added script to deploy through S3

---
+++ deploy-s3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+appname="DemoApplication"
+zipname="${appname}-$(date -u "+%Y%m%d-%H%M%S").zip"
+dgname="Demo-ASG-Ubuntu"
+bucket="default50-public"
+bucket_key="CodeDeploy"
+configname="CodeDeployDefault.OneAtATime"
+profile="code"
+
+aws deploy push --profile ${profile} --application-name ${appname} --s3-location s3://${bucket}/${bucket_key}/${zipname} --description ${appname}
+aws deploy create-deployment --profile ${profile} --application-name ${appname} --deployment-config-name ${configname} --deployment-group-name ${dgname} --s3-location bucket=${bucket},bundleType=zip,key=${bucket_key}/${zipname}
