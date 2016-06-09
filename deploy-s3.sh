5f51cd6f1febc6100ef00281dde1db1bf75f5166
Sebastian Cruz <cruzseba@amazon.com>
2016-06-09 18:02:50 +0100

    Added option to ignore ApplicationStop errors

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 appname="DemoApplication"
-dgname="Demo-ASG-Ubuntu"
+dgname="Demo-Tag-Ubuntu"
 zipname="${appname}-${dgname}-$(date -u "+%Y%m%d-%H%M%S").zip"
 bucket="default50-public"
 bucket_key="CodeDeploy"
-configname="CodeDeployDefault.OneAtATime"
+configname="CodeDeployDefault.AllAtOnce"
+#configname="CodeDeployDefault.OneAtATime"
 profile="code"
 
+if [ "x${1}" = "x-i" ]; then
+  IGNORE="--ignore-application-stop-failures"
+fi
+
 aws deploy push --profile ${profile} --application-name ${appname} --s3-location s3://${bucket}/${bucket_key}/${zipname} --ignore-hidden-files --description ${appname}
-aws deploy create-deployment --profile ${profile} --application-name ${appname} --deployment-config-name ${configname} --deployment-group-name ${dgname} --s3-location bucket=${bucket},bundleType=zip,key=${bucket_key}/${zipname}
+aws deploy create-deployment --profile ${profile} --application-name ${appname} --deployment-config-name ${configname} --deployment-group-name ${dgname} --s3-location bucket=${bucket},bundleType=zip,key=${bucket_key}/${zipname} ${IGNORE}
